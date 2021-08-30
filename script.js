eb0e4ebfde2b76d4e4224b01dc2f454a29e2188f
Israel Cena <israelsjm@gmail.com>
2021-08-30 12:19:50 -0300

    change code style

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const checkbox = document.getElementById('checkbox');
+const checkbox = document.getElementById("checkbox");
 
-checkbox.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-})
+checkbox.addEventListener("change", () => {
+	document.body.classList.toggle("dark");
+});
